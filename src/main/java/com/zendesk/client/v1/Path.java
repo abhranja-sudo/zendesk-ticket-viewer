3d7a6fd4a6b6c94dab30c3e6902783f72ced5d83
Abhishek R <abhranja@gmail.com>
2021-11-22 02:14:36 -0800

    feature_get_all_tickets | Adding url paths

---
+++ src/main/java/com/zendesk/client/v1/Path.java
@@ -0,0 +1,7 @@
+package com.zendesk.client.v1;
+
+public final class Path {
+    public static final String BASE_URL = "https://zccar.zendesk.com/";
+    public static final String API_VERSION = "api/v2/";
+    public static final String GET_ALL_TICKETS = "tickets.json/";
+}
