454ada8a5f516b15d78c0b224f20cf5965db7329
Abhishek R <abhranja@gmail.com>
2021-11-24 10:17:06 -0800

    feature_get_all_tickets | Main

---
+++ src/main/java/com/zendesk/client/v1/Main.java
@@ -0,0 +1,14 @@
+package com.zendesk.client.v1;
+
+import com.zendesk.client.v1.controller.Controller;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Main {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+
+            new Controller().run();
+
+    }
+}
