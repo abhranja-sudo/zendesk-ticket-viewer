731d4fd697702ddf2711472037c18c0410b3589b
Abhishek R <abhranja@gmail.com>
2021-11-24 00:06:25 -0800

    feature_get_all_tickets | Added Viewer for users to interact

---
+++ src/main/java/com/zendesk/client/v1/view/Viewer.java
@@ -0,0 +1,25 @@
+package com.zendesk.client.v1.view;
+
+import com.zendesk.client.v1.model.viewframe.Frame;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Viewer {
+
+    public String prompt(String prompt) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.print(prompt);
+            return br.readLine();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void display(Frame frame) {
+        System.out.println(frame);
+    }
+}
