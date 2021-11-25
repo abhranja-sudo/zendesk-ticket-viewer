06f96498e2a31164a8a8d4ee6644c49a25af3363
Abhishek R <abhranja@gmail.com>
2021-11-25 00:51:45 -0800

    feature_get_single_tickets | Added constant to accommodate single get feature

--- src/main/java/com/zendesk/client/v1/model/viewframe/ViewConstants.java
+++ src/main/java/com/zendesk/client/v1/model/viewframe/ViewConstants.java
@@ -4,14 +4,25 @@ import com.zendesk.client.v1.Input;
 
 public final class ViewConstants {
 
-    public static final String GREETING = String.format("%85s ", "*** Welcome ***\n");
-    public static final String APPNAME = String.format("%92s ", "*** Zendesk Ticket Viewer ***\n");
-    public static final String MENU = String.format("%5s %1s %1s ", "press", Input.MENU.getInput(), "for menu");
+    public static final String GREETING_VIEW = String.format("%85s ", "*** Welcome ***\n");
+    public static final String APP_NAME_VIEW = String.format("%92s ", "*** Zendesk Ticket Viewer ***\n");
+    public static final String MENU_VIEW = String.format("%5s %1s %1s ", "press", Input.MENU.getInput(), "for menu");
 
-    public static final String GETALLTICKET = String.format("%5s %1s %1s ", "press", Input.GET_ALL_TICKETS.getInput(),
-            "to checkout all ticket summary ");
+    public static final String GET_ALL_TICKET_VIEW = String.format("%5s %1s %1s ", "press", Input.GET_ALL_TICKETS.getInput(),
+            "to retrieve all ticket ");
 
-    public static final String QUIT = String.format("%5s %1s %1s ", "press", Input.QUIT.getInput(), "to quit");
+    public static final String GET_TICKET_VIEW = String.format("%5s %1s %1s ", "press", Input.GET_TICKET.getInput(), "to retrieve a ticket");
 
-    public static final String GET_NEXT = String.format("%5s %1s %1s ", "press", Input.NEXT.getInput(), "for next set of tickets ");
+    public static final String QUIT_VIEW = String.format("%5s %1s %1s ", "press", Input.QUIT.getInput(), "to quit");
+
+    public static final String GET_NEXT_VIEW = String.format("%5s %1s %1s ", "press", Input.NEXT.getInput(), "for next set of tickets ");
+
+    public static final String ENTER_TICKET_ID = "Enter Ticket ID : \n";
+
+    public static final String INVALID_INPUT = "Invalid Input, Please try Again\n";
+
+    public static final String END_OF_PAGE = String.format("%115s ",
+            "********** End of Page. Select Menu options below to continue *********** ");
+
+
 }
