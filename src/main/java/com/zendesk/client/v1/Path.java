7917c3f339a41dd37ca0a5e51b129e2f1572a2ef
Abhishek R <abhranja@gmail.com>
2021-11-24 23:17:50 -0800

    feature_get_single_tickets | Included url in Path for fetching single ticket

--- src/main/java/com/zendesk/client/v1/Path.java
+++ src/main/java/com/zendesk/client/v1/Path.java
@@ -4,4 +4,5 @@ public final class Path {
     public static final String BASE_URL = "https://zccar.zendesk.com/";
     public static final String API_VERSION = "api/v2/";
     public static final String GET_ALL_TICKETS = "tickets.json/";
+    public static final String GET_TICKET = "tickets/";
 }
