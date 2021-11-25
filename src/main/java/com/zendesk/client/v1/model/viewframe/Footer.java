f3bc8f5c09f5a2468ba0c47f9a8bdffbb2de53ab
Abhishek R <abhranja@gmail.com>
2021-11-25 00:48:48 -0800

    feature_get_single_tickets | refactored name and made Footer final

--- src/main/java/com/zendesk/client/v1/model/viewframe/Footer.java
+++ src/main/java/com/zendesk/client/v1/model/viewframe/Footer.java
@@ -3,30 +3,32 @@ package com.zendesk.client.v1.model.viewframe;
 import java.util.Objects;
 import java.util.Optional;
 
-public class Footer {
+public final class Footer {
 
     private final String menu;
-    private final String allTickets;
+    private final String getAllTickets;
+    private final String getTicket;
     private final String quit;
     private final String getNext;
-    private final String errorMessage;
+    private final String customMessage;
 
-    private Footer(String menu, String allTickets, String quit, String getNext, String errorMessage) {
+    private Footer(String menu, String getAllTickets, String getTicket, String quit, String getNext, String errorMessage) {
         this.menu = menu;
-        this.allTickets = allTickets;
+        this.getAllTickets = getAllTickets;
+        this.getTicket = getTicket;
         this.quit = quit;
         this.getNext = getNext;
-        this.errorMessage = errorMessage;
+        this.customMessage = errorMessage;
     }
 
     public static Footer.Builder builder() {
         return new Footer.Builder();
     }
 
-
     public static final class Builder {
         private String menu;
-        private String allTickets;
+        private String getAllTickets;
+        private String getTicket;
         private String quit;
         private String getNext;
         private String errorMessage;
@@ -45,14 +47,25 @@ public class Footer {
         }
 
 
-        public final Builder allTickets(String allTickets) {
-            this.allTickets = Objects.requireNonNull(allTickets, "allTickets");
+        public final Builder getAllTickets(String allTickets) {
+            this.getAllTickets = Objects.requireNonNull(allTickets, "allTickets");
             return this;
         }
 
 
-        public final Builder allTickets(Optional<String> allTickets) {
-            this.allTickets = allTickets.orElse(null);
+        public final Builder getAllTickets(Optional<String> allTickets) {
+            this.getAllTickets = allTickets.orElse(null);
+            return this;
+        }
+
+        public final Builder getTicket(String ticket) {
+            this.getTicket = Objects.requireNonNull(ticket, "ticket");
+            return this;
+        }
+
+
+        public final Builder getTicket(Optional<String> ticket) {
+            this.getTicket = ticket.orElse(null);
             return this;
         }
 
@@ -78,29 +91,31 @@ public class Footer {
             return this;
         }
 
-        public final Builder errorMessage(String errorMessage) {
+        public final Builder customMessage(String errorMessage) {
             this.errorMessage = Objects.requireNonNull(errorMessage, "quit");
             return this;
         }
 
 
-        public final Builder errorMessage(Optional<String> errorMessage) {
+        public final Builder customMessage(Optional<String> errorMessage) {
             this.errorMessage = errorMessage.orElse(null);
             return this;
         }
 
         public Footer build() {
-            return new Footer(menu, allTickets, quit, getNext, errorMessage);
+            return new Footer(menu, getAllTickets, getTicket, quit, getNext, errorMessage);
         }
     }
+
+
 
     @Override
     public String toString() {
 
         StringBuilder builder = new StringBuilder("\n");
 
-        if (errorMessage != null) {
-            builder.append(errorMessage + " \n");
+        if (customMessage != null) {
+            builder.append(customMessage + " \n\n");
         }
 
         if (getNext != null) {
@@ -111,8 +126,12 @@ public class Footer {
             builder.append(menu +" \n");
         }
 
-        if (allTickets != null) {
-            builder.append(allTickets +" \n");
+        if (getAllTickets != null) {
+            builder.append(getAllTickets +" \n");
+        }
+
+        if(getTicket != null) {
+            builder.append(getTicket +" \n");
         }
 
         if (quit != null) {
