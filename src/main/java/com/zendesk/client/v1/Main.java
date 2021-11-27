3101d02838e37adc9b8341a5f065e6f166ccdffc
Abhishek R <abhranja@gmail.com>
2021-11-26 18:01:17 -0800

    Removed unnecessary code

--- src/main/java/com/zendesk/client/v1/Main.java
+++ src/main/java/com/zendesk/client/v1/Main.java
@@ -2,11 +2,8 @@ package com.zendesk.client.v1;
 
 import com.zendesk.client.v1.controller.Controller;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 public class Main {
-    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+    public static void main(String[] args) {
 
             new Controller().run();
 
